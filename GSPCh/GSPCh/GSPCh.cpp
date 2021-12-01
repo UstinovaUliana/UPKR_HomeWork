ee7771553bc728826e0aeac7984f76150187a177
Uliana <mirrorrune21363@gmail.com>
2021-12-01 21:25:49 +0300

    Заготовка для запуска алгоритмов; вычисления для определения качества результата (начато)

--- GSPCh/GSPCh/GSPCh.cpp
+++ GSPCh/GSPCh/GSPCh.cpp
@@ -1,20 +1,69 @@
-﻿// GSPCh.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <vector>
+#include <chrono>
 #include <iostream>
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
 
+int getInt()
+{
+    int input;
+    while (1) {
+        cin >> input;
+        if (!cin.fail())
+            return input;
+        cin.clear();
+        cin.ignore(2000, '\n');
+        cout << "Ошибка, повторите ввод" << endl;
+    }
+}
+using alg = vector<int>(*)(int dano);
+
+void checkAlg(string name, alg algoritm, int dano[3]) {
+    auto start = chrono::system_clock::now();
+    vector<int> result = algoritm(dano[3]);
+    auto stop = chrono::system_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start).count();
+    int sumres = 0;
+    for (auto i : result) {
+        cout << i << " ";
+        sumres += i;
+    }
+    //равномерное распределение
+    float raspr = 1./dano[3]-dano[2];
+    float srper; //средний период
+    float srznach=sumres/ result.size(); //среднее значение
+    int s=result.size()-1;
+    vector <int> di;
+    for (int i = 1; i < result.size(); i++) {
+        // map <i, appearence count> ;
+        //
+
+        di.push_back(result[i + 1] - result[i]);
+        int sumdi=0;
+        for (auto j : di) {
+            sumdi += j;
+        }
+        srper = sumdi / s;
+    }
+
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    int kolvo;
+    int ngd;
+    int vgd;
+    cout << "Введите количество случайных чисел: ";
+    kolvo = getInt();
+    cout <<endl << "Введите диапазон: ";
+    cout << endl << "От: ";
+    ngd = getInt();
+    cout << endl << "До: ";
+    vgd = getInt();
+    int dano[3] = { kolvo,ngd,vgd };
+
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
