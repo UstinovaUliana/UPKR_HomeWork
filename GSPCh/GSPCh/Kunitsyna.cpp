3d1c7ca341e939bdbe402f46e472fb61b1db20ae
rlftr <rlftr@yandex.ru>
2021-12-05 22:29:05 +0300

    done2

--- GSPCh/GSPCh/Kunitsyna.cpp
+++ GSPCh/GSPCh/Kunitsyna.cpp
@@ -5,20 +5,19 @@
 #include <math.h>
 using namespace std;
 
-int p = 111;
-int q = 77;
-int x0 = 13;
-int m = p * q;
-int kolvo, i, random, ngd, vgd;
-
 int RandNum(int& x0, int m) {
 	int nextRandNum = (x0 * x0) % m;
 	x0 = nextRandNum;
 	return nextRandNum;
 }
 
-vector <int> algorithm() {
-	vector <int> BBS = {};
+vector <float> BBS_algorithm(int kolvo, float ngd, float vgd, float scale) {
+	vector <float> BBS;
+	int random;
+	int p = 111;
+	int q = 77;
+	int x0 = 13;
+	int m = p * q;
 	cout << "Blum-Blum-Shub algorithm\n";
 	cout << "Generate ";
 	cout << kolvo;
@@ -26,9 +25,13 @@ vector <int> algorithm() {
 
 	for (int i = 0; i < kolvo; i++) {
 		random = ngd + RandNum(x0, m) % vgd;
-		cout << random << " ";
 		BBS.push_back((float)random);
 	}
 	cout << endl;
+
+	for (auto& i : BBS) {
+		i = floor(i * scale) / scale;
+	}
+
 	return BBS;
 }
