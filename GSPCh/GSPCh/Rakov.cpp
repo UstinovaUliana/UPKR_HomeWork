bb628c4192d5327ecbe39bee73f64e8bbde760fa
1sensus <miraxlxd@gmail.com>
2021-12-05 20:58:01 +0300

    Update Rakov.cpp

--- GSPCh/GSPCh/Rakov.cpp
+++ GSPCh/GSPCh/Rakov.cpp
@@ -11,7 +11,7 @@ vector <float> Psevdo(const int& arr_size)
     for (int a = 0; a < arr_size; a++)
     {
         Y_rand = (float)rand() / (float)RAND_MAX;
-        digit = -1 * ((float)log(Y_rand) / (float)log(5)) / lyambda;//X=logY/log5
+        digit = -1 * ((float)log(Y_rand) / (float)log(5)) / lyambda;///X=logY/log5
         P_arr.push_back(digit);
     }
     return P_arr;
